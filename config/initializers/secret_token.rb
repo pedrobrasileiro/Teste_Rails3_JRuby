08503e464de6f3df6dfcf390d04d94af9d56b5dc
Pedro Brasileiro <pedrobrasileiro@gmail.com>
2011-08-20 14:40:25 -0300

    Gerando App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TesteRails3JRuby::Application.config.secret_token = 'f8a6e8e09bee84652a10974fe9b52cc1ac5e055b0e3ce7db049348ee92d58194c32b619cce88c3d974b2626ddcba74d0665df6ce33383fb95d04c8889ce8c518'
